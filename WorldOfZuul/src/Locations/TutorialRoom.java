60ec3483b14fa2d38ff7b989a229d470fc692d4a
Michael Pedersen <Michael@michado.dk>
2018-10-26 14:23:47 +0200

    finished tutorial

--- WorldOfZuul/src/Locations/TutorialRoom.java
+++ WorldOfZuul/src/Locations/TutorialRoom.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 public class TutorialRoom extends Room {
 
     Scanner input = new Scanner(System.in);
-    boolean answer;
+    boolean answer, correktOption;
     private final Trailer trailer;
+    
 
     public TutorialRoom(String description, Player player, Trailer trailer) {
 
@@ -46,9 +47,34 @@ public class TutorialRoom extends Room {
                 + "option 2: karatechop a twig\n"
                 + "Try typing 'option' followed by the choise you would like to make\n");
             String option = input.nextLine();
+            correktOption = true;
+            while(correktOption){
+                switch(option){
+                    case "option 1" : 
+                        System.out.println("Great you picked up a rock");
+                        correktOption = false; break;
+                    case "option 2" : 
+                        System.out.println("You made a ferius karatechop and choped a twig in half\n"
+                            + "Be proud of yourself!");
+                        correktOption = false; break; 
+                    default : option = input.nextLine();
+                }
+            }
+            System.out.println("GREAT!\n"
+                + "Now you're almost done with the tutorial!\n"
+                + "before you wake up back in your trailer we would like to remind you that "
+                + "your world has a noth, west, east and south area you can go to\n"
+                + "Be sure to explore as you please!\n"
+                + "Good gaming!");
             
 
-            return getExitString();
+            return "You are standing inside your trailer!\n"
+                + "This is your home, you have " + humanPlayer.getClimatePoints() + " climate points,"
+                + " your options are: \n"
+                + "Option 1 - Load off logs you are carrying \n"
+                + "Option 2 - Look in your wallet \n"
+                + "Option 3 - Sleep"
+                + getExitString();
         } else {
 
             humanPlayer.setCurrentRoom(trailer);
