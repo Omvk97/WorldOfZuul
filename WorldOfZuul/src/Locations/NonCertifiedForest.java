a6afc832c461702e6a1a18a04367a9511092d67e
Michael Pedersen <Michael@michado.dk>
2018-11-06 13:41:52 +0100

    Revert "Option edited"
    This reverts commit 6670e45f9b53417c0e57839b471508be7b7b3207.

--- WorldOfZuul/src/Locations/NonCertifiedForest.java
+++ WorldOfZuul/src/Locations/NonCertifiedForest.java
@@ -24,10 +24,8 @@ public class NonCertifiedForest extends Forest {
         return "You are standing " + getShortDescription() + "!\n"
             + "This forest will not regrow, there are " + trees.size() + " trees" + "\n"
             + "Your options are: \n"
-            + "----------------------------------\n"
             + "Option 1 - Cut down a tree and bring it with you \n"
-            + "Option 2 - See how many trees are left in the forest"
-            + "----------------------------------\n";
+            + "Option 2 - See how many trees are left in the forest";
     }
 
     @Override
