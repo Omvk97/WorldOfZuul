01f3233a9426a817c3465bb2b3605a52c6d40c8f
Steffen Vitten Pedersen <stped18@student.sdu.dk>
2018-12-06 20:21:55 +0100

    exit handler

--- WorldOfZuul/src/controllers/StartSceneController.java
+++ WorldOfZuul/src/controllers/StartSceneController.java
@@ -7,30 +7,24 @@ package controllers;
 
 import game_functionality.Game;
 import game_functionality.Player;
-import game_locations.Trailer;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author steff
  */
-public class StartSceneController implements Initializable{ 
+public class StartSceneController implements Initializable {
+
     private final Player humanPlayer = Game.getInstanceOfSelf().getHumanPlayer();
-  
+
     @FXML
     private AnchorPane anchorPane;
     @FXML
@@ -39,32 +33,27 @@ public class StartSceneController implements Initializable{
     private Button btnOptionhandler;
     @FXML
     private Button btnEXIT;
-    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-      
-    }    
 
-    @FXML
-    private void HandleStartGameAction(ActionEvent event) {
-
-        anchorPane.getScene().setRoot(humanPlayer.getCurrentRoom().getRoomFXML());
     }
 
     @FXML
-    private void optionAction(ActionEvent event) {
-        
+    private void HandleStartGameAction(ActionEvent event) {
+        anchorPane.getScene().setRoot(humanPlayer.getCurrentRoom().getRoomFXML());
     }
 
     @FXML
     private void ExitAction(ActionEvent event) {
         System.exit(0);
     }
-    
-}
 
+    @FXML
+    private void handlerHighscore(MouseEvent event) {
+    }
+
+}
